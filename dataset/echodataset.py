190fffcdf1749c04c0aa78965ce674c101dc2bb5
EngEmmanuel <emmanueloladokun@hotmail.co.uk>
2025-08-29 14:31:19 +0100

    Updates to echodataset

--- dataset/echodataset.py
+++ dataset/echodataset.py
@@ -18,7 +18,7 @@ class EchoDataset(Dataset):
         self.device = device
         self.data_path = Path(cfg.dataset.path)
         self.df = pd.read_csv(self.data_path / 'metadata.csv')
-        self.df = self.df[self.df['split'] == split].reset_index(drop=True)
+        self.df = self.df[self.df['split'].str.lower() == split].reset_index(drop=True)
 
         self.shape = self.cfg.dataset.get('shape', None)
         if self.shape is None:
@@ -30,46 +30,47 @@ class EchoDataset(Dataset):
     def __len__(self):
         return len(self.df)
 
-    def resample_sequence(frames: np.ndarray | torch.Tensor, target_length: int = 32) -> np.ndarray | torch.Tensor:
+    def resample_sequence(self, frames: torch.Tensor, target_length: int = 32) -> np.ndarray | torch.Tensor:
         """
         Resample a sequence to a target length.
-        
         Args:
-            frames: Input frames of shape (T, H, W) or (T, H, W, C)
+            frames: Input frames of shape (T, C, H, W)
             target_length: Target number of frames
-            
         Returns:
-            Resampled frames of shape (target_length, H, W) or (target_length, H, W, C)
-            
+            Resampled frames of shape (target_length, C, H, W)
+            and a padding mask of shape (target_length, 1, H, W)
         Notes:
             - If T < target_length, append zero-padded frames
             - If T >= target_length, evenly sample frames including endpoints
         """
-
-        lib = torch if isinstance(frames, torch.Tensor) else np
-
-        T = frames.shape[0]
+        T, *data_shape = frames.shape
         if T < target_length:
             # Pad with zeros
             pad_shape = (target_length - T,) + frames.shape[1:]
-            pad_block = lib.zeros(pad_shape, dtype=frames.dtype)
-            return lib.concatenate([frames, pad_block], axis=0)
+            pad_block = torch.zeros(pad_shape, dtype=frames.dtype)
+            resampled_frames = torch.cat([frames, pad_block], dim=0)
+            pad_mask = torch.cat(
+                [torch.zeros((T, 1, *data_shape[1:]), dtype=frames.dtype),
+                 torch.ones((target_length - T, 1, *data_shape[1:]), dtype=frames.dtype)
+                 ],
+                dim=0)
         else:
             # Evenly sample frames including endpoints
-            indices = lib.linspace(0, T - 1, target_length)
-            indices = lib.round(indices).astype(int)
-            return frames[indices]
-        
+            indices = torch.linspace(0, T - 1, target_length)
+            indices = torch.round(indices).to(torch.int64)
+            resampled_frames = frames[indices]
+            pad_mask = torch.zeros_like((target_length, 1, *data_shape[1:]), dtype=frames.dtype)
 
-    def mask_random_frames(self, video: torch.Tensor, t:int):
+        return resampled_frames, pad_mask
+
+    def mask_random_frames(self, video: torch.Tensor):
         '''
         Masks 'n_missing_frames' in the range [0, t) of the input video tensor.
         '''
-        T, C, H, W = video.shape
-        assert t <= T, "t must be less than or equal to T"
+        T = video.shape[0]
 
-        n_missing_frames = torch.randint(low=1, high=t, size=(1,)).item() #[1,t)i.e., Upper bound not inclusive therefore t-1 is max
-        mask_indices = random.sample(range(t), n_missing_frames)
+        n_missing_frames = torch.randint(low=1, high=T, size=(1,)).item() #[1,t)i.e., Upper bound not inclusive therefore t-1 is max
+        mask_indices = random.sample(range(T), n_missing_frames)
 
         mask = torch.ones_like(video)
         mask[mask_indices, ...] = 0
@@ -83,28 +84,35 @@ class EchoDataset(Dataset):
 
     def transform(self, z: torch.Tensor):
         # randomly mask frames
-        z_masked, observed_mask = self.mask_random_frames(z, t=self.cfg.dataset.max_frames)
-        #TODO: Look at adding masking to communicate missing/padded regions
-        #z_masked = torch.cat([z_masked, observed_mask], dim=1) # concatenate along channel dimension
+        z_masked, observed_mask = self.mask_random_frames(z)
 
+        #z.shape == z_masked.shape = (t, C, H, W)
         # downsample or pad up to T frames
-        z = self.resample_sequence(z, target_length=self.cfg.dataset.max_frames)
-        z_masked = self.resample_sequence(z_masked, target_length=self.cfg.dataset.max_frames)
+        resampled, pad_mask = self.resample_sequence(
+            torch.cat([z, z_masked, observed_mask], dim=1),
+            target_length=self.cfg.dataset.max_frames
+        )
 
-        return z, z_masked
+        z, cond = resampled[:,:self.shape[1],...], resampled[:,self.shape[1]:,...]
+        cond = torch.cat([cond, pad_mask], dim=1)
+
+        return z, cond
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
 
-        stats = torch.load(self.data_path / f"{row['video_name']}.pt", map_location=self.device)
+        stats = torch.load(self.data_path / f"{row['video_name']}.pt")
         mu, var = stats['mu'], stats['var'] #(T, C, H, W)
 
-        eps = torch.randn_like(mu, device=self.device)
+        eps = torch.randn_like(mu)
         z = mu + var.sqrt() * eps
-        z, z_masked = self.transform(z)
+        z, cond = self.transform(z)
 
-        ef = row['ef']
-        ehs_dim = self.cfg.model.kwargs.get('cross_attention_dim', self.cfg.model.kwargs.caption_channels)
-        encoder_hidden_states = ef.view(1, 1).expand(1, ehs_dim)
+        z = z.to(self.device)
+        cond = cond.to(self.device)
 
-        return {"x": z, "cond_image": z_masked, "encoder_hidden_states": encoder_hidden_states}
+        ef = torch.tensor(row['EF_Area'], device=self.device)
+        ehs_dim = self.cfg.model.kwargs.get('cross_attention_dim', self.cfg.model.kwargs.get('caption_channels'))
+        encoder_hidden_states = ef.view(1, 1).expand(1, ehs_dim).to(self.device)
+
+        return {"x": z, "cond_image": cond, "encoder_hidden_states": encoder_hidden_states}
